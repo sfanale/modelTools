bfc53e89ca1f629d16b53b1c1f5e4b002a984dfb
stephen <stephenfanale@hotmail.com>
2018-10-27 18:42:10 -0400

    -- added port.optimize (still in progress)

--- Portfolio.py
+++ Portfolio.py
@@ -9,6 +9,7 @@ class Portfolio:
     def __init__(self):
         self.holdings = {}
 
+
     def add(self, price):
         if price['contractsymbol'] not in self.holdings:
             self.holdings['contractsymbol'] =[{'date': price['pricedate'], 'strike': price['strike'],
@@ -20,11 +21,20 @@ class Portfolio:
                                                'type': price['optiontype']})
 
 
+    def optimize(self, start_date, end_date ):
+        w0 = np.ones(len(self.holdings)) / len(self.holdings)    # initial weights equal
+        for stock in self.holdings:
+            temp = sorted(stock, key=lambda item: item['date'])   # sort prices in contract array by date
+            prices = [temp[i]['price'] for i in range(len(temp))]
+            returns = [temp[i]/temp[i-1] -1 for i in range(1, len(prices))]
+
+
+
 
 def sharpe_ratio(w,Ri, Rave, Rf):
     return np.sqrt(np.dot(np.transpose(w), np.dot(np.cov(Ri), w))) / (np.dot(np.transpose(w), Rave) - Rf)
 
 
-w0 = np.ones(len(Portfolio.holdings))/len(Portfolio.holdings)
+
 
 result = sp.minimize(sharpe_ratio, w0, args=(Ri, Rave, Rf))
